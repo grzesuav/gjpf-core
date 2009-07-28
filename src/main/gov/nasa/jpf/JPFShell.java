85f633cea0c433c737f36de09d46eaf2b87503f9
Peter Mehlitz <Peter.C.Mehlitz@nasa.gov>
2009-07-28 13:39:35 -0700

    * added comments reg. JPFShell justification * fixed NB debugging problem (using cached .netbeans sourcepath) * fixed NB syntax check classpath

--- src/main/gov/nasa/jpf/JPFShell.java
+++ src/main/gov/nasa/jpf/JPFShell.java
@@ -27,7 +27,15 @@ package gov.nasa.jpf;
  * Instantiation and start() call are done from the JPF.main() method
  *
  * Usually, JPFShell implementors have a <init>(Config) ctor so that they
- * don't need to obtain a new Config object
+ * don't need to obtain a new Config object. This is the reason for
+ * using a shell instance instead of a static main() method - we cannot
+ * pass the Config object that was already created by JPF.main() at this point
+ * into the shell main(), which means we would either have to turn Config
+ * into a singleton (causing problems for multiple JPF runs), or create a new
+ * Config object within the shell. Since initialization of Config objects is
+ * an expensive task with our new bootstrapping, it seems better to pass the
+ * Config object from JPF.main() (which is done automatically when using
+ * Config based initialization)
  */
 public interface JPFShell {
 
