5cadbae66c93ac924177307496931e011002d7b8
nathanila <none@none>
2010-08-24 15:03:40 -0700

    Implemented most of java.util.regex.Matcher methods.

--- src/classes/java/util/regex/Matcher.java
+++ src/classes/java/util/regex/Matcher.java
@@ -34,6 +34,9 @@ public class Matcher {
   Pattern pattern;
   String input;    // that's an approximation (don't use CharSequence on the native side)
   
+  Matcher() {
+  }
+  
   Matcher (Pattern pattern, CharSequence inp){
     this.pattern = pattern;
     this.input = inp.toString();
@@ -41,11 +44,21 @@ public class Matcher {
     register();
   }
   
+  public Pattern pattern() {
+    return(pattern);
+  }
+  
   native void register();
   
   public native Matcher reset();
   
+  public String group() {
+     return(group(0));
+  }
+  
   public native String group(int group);
+  
+  public native int groupCount();
 
   public Matcher reset(CharSequence inp) {
     this.input = inp.toString();
@@ -56,9 +69,47 @@ public class Matcher {
   
   public native boolean find();
   
-  public native int end();
+  public native boolean lookingAt();
+  
+  public int start() {
+     return(start(0));
+  }
+  
+  public native int start(int group);
+  
+  public int end() {
+     return(end(0));
+  }
+  
+  public native int end(int group);
+
+  public native boolean hasTransparentBounds();
+
+  public native Matcher useTransparentBounds(boolean b);
+
+  public native boolean hasAnchoringBounds();
+
+  public native Matcher useAnchoringBounds(boolean b);
+
+  public native int regionStart();
+  
+  public native int regionEnd();
+
+  public native Matcher region(int start, int end);
 
   public static native String quoteReplacement(String abc);
 
   public native String replaceAll(String replacement);
+  
+  public native String replaceFirst(String replacement);
+  
+  public native String toString();
+  
+  public native boolean hitEnd();
+  
+  public native boolean requireEnd();
+
+  // TODO public native MatchResult toMatchResult();
+  // TODO public native StringBuffer appendTail(StringBuffer sb);
+  // TODO public native Matcher appendReplacement(StringBuffer sb, String replacement);
 }
