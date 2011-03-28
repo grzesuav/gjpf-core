02214d5af2517f9c085264016c82f160d3a02930
Ivan Mushektik <proger.home@gmail.com>
2011-03-28 22:08:14 +0300

    RepositoryEntry updated. RepositoryEntry refactored. Support for hg and git added.

--- src/main/gov/nasa/jpf/util/RepositoryEntry.java
+++ src/main/gov/nasa/jpf/util/RepositoryEntry.java
@@ -41,61 +41,25 @@ public class RepositoryEntry {
   String repository;
   String revision;
 
+  static RepositoryEntryFactory searchers[] = {
+    new SvnRepositoryEntryFactory(),
+    new HgRepositoryEntryFactory(),
+    new GitRepositoryEntryFactory()
+  };
+
   public static RepositoryEntry getRepositoryEntry (String fullFileName) {
     RepositoryEntry e = dict.get(fullFileName);
     
     if (e == null) {
-      if ((e = getSvnRepositoryEntry(fullFileName)) == null) {
-        // <2do> check CVS and others here
+      for (RepositoryEntryFactory factory : searchers) {
+        if ((e = factory.getRepositoryEntry(fullFileName)) != null)
+          break;
       }
     }
     
     dict.put(fullFileName, e); // no need to look this up more than once
     return e;
   }
-  
-  /*
-   * <2do> doesn't work on Windows, where the .svn/entries is apparently
-   * not stored as an XML file
-   */
-  static RepositoryEntry getSvnRepositoryEntry (String fullFileName) {
-    File f = new File(fullFileName);
-    String fname = f.getName();
-    String dName = f.getParent();
-    
-    File fEntries = new File(dName + File.separatorChar + ".svn" + File.separatorChar + "entries");
-    if (fEntries.exists()) {
-      String repository = "?";
-      String revision = "?";
-      
-      Pattern pName = Pattern.compile(" *name=\"([a-zA-Z0-9.]+)\"");
-      Pattern pRep = Pattern.compile(" *url=\"([a-zA-Z0-9.:/\\-]+)\"");
-      Pattern pRev = Pattern.compile(" *committed-rev=\"([0-9]+)\"");
-      try {
-        BufferedReader r = new BufferedReader(new FileReader(fEntries));
-        for (String line=r.readLine(); line != null; line = r.readLine()) {
-          Matcher mRep = pRep.matcher(line);
-          if (mRep.matches()) {
-            repository = mRep.group(1);
-          } else {
-            Matcher mRev = pRev.matcher(line);
-            if (mRev.matches()) {
-              revision = mRev.group(1);
-            } else {
-              Matcher mName = pName.matcher(line);
-              if (mName.matches() && mName.group(1).equals(fname)) {
-                // Ok, got everything
-                return new RepositoryEntry(fname, "svn", repository, revision);
-              }
-            }
-          }
-        }
-      } catch (Throwable t) {}
-    }
-    
-    return null;
-  }
-
   
   public RepositoryEntry (String fileName, String repositoryType, String repository, String revision) {
     this.fileName = fileName;
@@ -120,4 +84,155 @@ public class RepositoryEntry {
     return revision;
   }
 
+}
+
+interface RepositoryEntryFactory {
+  RepositoryEntry getRepositoryEntry(String fullFileName);
+}
+
+class SvnRepositoryEntryFactory implements RepositoryEntryFactory {
+
+  /*
+   * <2do> doesn't work on Windows, where the .svn/entries is apparently
+   * not stored as an XML file
+   */
+  public RepositoryEntry getRepositoryEntry(String fullFileName) {
+    File f = new File(fullFileName);
+    String fname = f.getName();
+    String dName = f.getParent();
+
+    File fEntries = new File(dName + File.separatorChar + ".svn" + File.separatorChar + "entries");
+    if (fEntries.exists()) {
+      String repository = "?";
+      String revision = "?";
+
+      Pattern pName = Pattern.compile(" *name=\"([a-zA-Z0-9.]+)\"");
+      Pattern pRep = Pattern.compile(" *url=\"([a-zA-Z0-9.:/\\-]+)\"");
+      Pattern pRev = Pattern.compile(" *committed-rev=\"([0-9]+)\"");
+      try {
+        BufferedReader r = new BufferedReader(new FileReader(fEntries));
+        for (String line=r.readLine(); line != null; line = r.readLine()) {
+          Matcher mRep = pRep.matcher(line);
+          if (mRep.matches()) {
+            repository = mRep.group(1);
+          } else {
+            Matcher mRev = pRev.matcher(line);
+            if (mRev.matches()) {
+              revision = mRev.group(1);
+            } else {
+              Matcher mName = pName.matcher(line);
+              if (mName.matches() && mName.group(1).equals(fname)) {
+                // Ok, got everything
+                return new RepositoryEntry(fname, "svn", repository, revision);
+              }
+            }
+          }
+        }
+      } catch (Throwable t) {}
+    }
+
+    return null;
+  }
+}
+
+class HgRepositoryEntryFactory implements RepositoryEntryFactory {
+
+  public RepositoryEntry getRepositoryEntry(String fullFileName) {
+    File file = new File(fullFileName);
+
+    if (!file.exists())
+      return null;
+
+    File currentDir = file.getParentFile();
+
+    searchForHg:
+    while (currentDir != null) {
+      for (String childName : currentDir.list())
+        if (childName.equals(".hg"))
+          break searchForHg;
+
+      currentDir = currentDir.getParentFile();
+    }
+
+    if (currentDir != null) {
+      try {
+        File hgrcFile = new File(currentDir, ".hg/hgrc");
+        
+        String repoURL = "";
+        BufferedReader r = new BufferedReader(new FileReader(hgrcFile));
+        for (String line=r.readLine(); line != null; line = r.readLine()) {
+          String keyVal[] = line.split("=");
+          if (keyVal[0].trim().equals("default")) {
+            repoURL = keyVal[1].trim();
+            break;
+          }
+
+        }
+
+        File branchHeads = new File(currentDir, ".hg/branchheads.cache");
+        r = new BufferedReader(new FileReader(branchHeads));
+        String revision = r.readLine().split(" ")[1];
+
+        return new RepositoryEntry(fullFileName, "hg", repoURL, revision);
+      }
+      catch (Exception ex) {
+        return null;
+      }
+    }
+
+    return null;
+  }
+  
+}
+
+class GitRepositoryEntryFactory implements RepositoryEntryFactory {
+
+  public RepositoryEntry getRepositoryEntry(String fullFileName) {
+    File file = new File(fullFileName);
+
+    if (!file.exists())
+      return null;
+
+    File currentDir = file.getParentFile();
+
+    searchForHg:
+    while (currentDir != null) {
+      for (String childName : currentDir.list()) {
+        if (childName.equals(".git")) {
+          break searchForHg;
+        }
+      }
+
+      currentDir = currentDir.getParentFile();
+    }
+
+    if (currentDir != null) {
+      try {
+        File hgrcFile = new File(currentDir, ".git/config");
+
+        String repoURL = "";
+        BufferedReader r = new BufferedReader(new FileReader(hgrcFile));
+        for (String line = r.readLine(); line != null; line = r.readLine()) {
+          String keyVal[] = line.split("=");
+          if (keyVal[0].trim().equals("url")) {
+            repoURL = keyVal[1].trim();
+            break;
+          }
+
+        }
+
+        // git doesn't has revision numbers so we will read last revision's hash instead
+        File gitHeadHash = new File(currentDir, ".git/refs/heads/master");
+        r = new BufferedReader(new FileReader(gitHeadHash));
+        String revision = r.readLine();
+
+        return new RepositoryEntry(fullFileName, "git", repoURL, revision);
+
+      } catch (Exception ex) {
+        return null;
+      }
+    }
+
+    return null;
+  }
 }
