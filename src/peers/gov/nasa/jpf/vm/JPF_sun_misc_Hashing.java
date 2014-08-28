752d6c685007f4b09bcc258dd33615c6ae0ed3a4
Peter Mehlitz <Peter.C.Mehlitz@nasa.gov>
2014-08-27 17:55:36 -0700

    reverted the sun.misc.Hashing peer to statically delegate to sun.misc.Hashing (which has been removed from Java 8)

--- src/peers/gov/nasa/jpf/vm/JPF_sun_misc_Hashing.java
+++ src/peers/gov/nasa/jpf/vm/JPF_sun_misc_Hashing.java
@@ -20,75 +20,42 @@
 package gov.nasa.jpf.vm;
 
 import gov.nasa.jpf.annotation.MJI;
-import java.lang.invoke.MethodHandle;
-import java.lang.invoke.MethodHandles;
-import java.lang.invoke.MethodType;
+import sun.misc.Hashing;
 
 /**
- * simple forwarding sun.misc.Hashing peer to speed up execution and shorten traces, especially
- * for system initialization
- * 
- * NOTE - this peer is only used under Java 7. Since Java 8 doesn't have a sun.misc.Hashing class,
- * we have to use reflection and MethodHandles for delegation
+ * simple forwarding sun.misc.Hashing peer to speed up execution and shorten traces
  */
 public class JPF_sun_misc_Hashing extends NativePeer {
     
-  MethodHandle byte_murmur3_32;
-  MethodHandle char_murmur3_32;
-  MethodHandle int_murmur3_32;
-  MethodHandle stringHash32;
-  MethodHandle randomHashSeed;
-  
-  
-  public JPF_sun_misc_Hashing () throws ClassNotFoundException, NoSuchMethodException, IllegalAccessException {
-    Class<?> hashingCls = Class.forName("sun.misc.Hashing");
-    MethodHandles.Lookup mhLookup = MethodHandles.lookup();
-
-    MethodType mt = MethodType.methodType( int.class,  int.class,byte[].class,int.class,int.class);
-    byte_murmur3_32 = mhLookup.findStatic( hashingCls, "murmur3_32", mt);
-    
-    mt = MethodType.methodType(int.class,  int.class,char[].class,int.class,int.class);
-    char_murmur3_32 = mhLookup.findStatic( hashingCls, "murmur3_32", mt);
-
-    mt = MethodType.methodType(int.class,  int.class,int[].class,int.class,int.class);
-    int_murmur3_32 = mhLookup.findStatic( hashingCls, "murmur3_32", mt);
-
-    mt = MethodType.methodType(int.class, String.class);
-    stringHash32 = mhLookup.findStatic( hashingCls, "stringHash32", mt);
-    
-    mt = MethodType.methodType(int.class, Object.class);
-    randomHashSeed = mhLookup.findStatic( hashingCls, "randomHashSeed", mt);
-  }
-  
   @MJI 
-  public int murmur3_32__I_3BII__I (MJIEnv env, int clsRef, int seed, int dataRef, int offset, int len) throws Throwable {
+  public int murmur3_32__I_3BII__I (MJIEnv env, int clsRef, int seed, int dataRef, int offset, int len){
     byte[] data = env.getByteArrayObject(dataRef);
-    return (Integer)byte_murmur3_32.invoke( seed, data, offset, len);
+    return Hashing.murmur3_32(seed, data, offset, len);
   }
   
   @MJI 
-  public int murmur3_32__I_3CII__I (MJIEnv env, int clsRef, int seed, int dataRef, int offset, int len) throws Throwable {
+  public int murmur3_32__I_3CII__I (MJIEnv env, int clsRef, int seed, int dataRef, int offset, int len){
     char[] data = env.getCharArrayObject(dataRef);
-    return (Integer)char_murmur3_32.invoke( seed, data, offset, len);
+    return Hashing.murmur3_32(seed, data, offset, len);
   }
   
   @MJI 
-  public int murmur3_32__I_3III__I (MJIEnv env, int clsRef, int seed, int dataRef, int offset, int len) throws Throwable {
+  public int murmur3_32__I_3III__I (MJIEnv env, int clsRef, int seed, int dataRef, int offset, int len){
     int[] data = env.getIntArrayObject(dataRef);
-    return (Integer)int_murmur3_32.invoke( seed, data, offset, len);
+    return Hashing.murmur3_32(seed, data, offset, len);
   }
 
   @MJI
-  public int stringHash32__Ljava_lang_String_2__I (MJIEnv env, int clsRef, int sRef) throws Throwable {
+  public int stringHash32__Ljava_lang_String_2__I (MJIEnv env, int clsRef, int sRef){
     String s = env.getStringObject(sRef);
-    return (Integer)stringHash32.invoke(s);
+    return Hashing.stringHash32(s);
   }
   
   @MJI
-  public int randomHashSeed__Ljava_lang_Object_2__I (MJIEnv env, int clsRef, int objRef) throws Throwable {
+  public int randomHashSeed__Ljava_lang_Object_2__I (MJIEnv env, int clsRef, int objRef){
     // <2do> since the original implementation uses things like System.currentTimeMillis()
     // we have to model this to make it reproducible between JPF runs
-    return (Integer)randomHashSeed.invokeExact(Integer.valueOf(objRef));
+    return Hashing.randomHashSeed( Integer.valueOf(objRef));
   }
 
 }
