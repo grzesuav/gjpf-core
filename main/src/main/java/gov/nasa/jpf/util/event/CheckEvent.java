46de4f45952083031b3252868a0ea3f0efb3ea70
grzesuav <grzesuav@gmail.com>
2015-03-14 00:01:24 +0100

    Add modular layout + maven + dist

---
+++ main/src/main/java/gov/nasa/jpf/util/event/CheckEvent.java
@@ -0,0 +1,91 @@
+/*
+ * Copyright (C) 2014, United States Government, as represented by the
+ * Administrator of the National Aeronautics and Space Administration.
+ * All rights reserved.
+ *
+ * The Java Pathfinder core (jpf-core) platform is licensed under the
+ * Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ * 
+ *        http://www.apache.org/licenses/LICENSE-2.0. 
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and 
+ * limitations under the License.
+ */
+
+package gov.nasa.jpf.util.event;
+
+import gov.nasa.jpf.vm.MJIEnv;
+
+/**
+ * a pseudo event that encapsulates a (possibly composed) check
+ * 
+ * This event type uses 'alt' for disjunction and 'next' for conjunction if
+ * they point to CheckEvents
+ */
+public abstract class CheckEvent extends Event {
+  
+  protected CheckEvent (String name, Object... arguments){
+    super(name, arguments);
+  }
+  
+  public abstract boolean evaluate(MJIEnv env);
+  
+  public boolean check (MJIEnv env){
+    if (!evaluate(env)){
+      if (alt != null && alt instanceof CheckEvent){
+        return ((CheckEvent)alt).check(env);
+      } else {
+        return false;
+      }
+      
+    } else {
+      if (next != null && next instanceof CheckEvent){
+        return ((CheckEvent)next).check(env);
+      } else {
+        return true;
+      }
+    }
+  }
+  
+  public CheckEvent or (CheckEvent orCheck){
+    addAlternative(orCheck);
+    
+    return this;
+  }
+  
+  public CheckEvent and (CheckEvent andCheck){
+    addNext( andCheck);
+    
+    return this;
+  }
+  
+  public String getExpression(){
+    if (alt == null && !(next instanceof CheckEvent)){
+      return toString();
+      
+    } else {
+      StringBuilder sb = new StringBuilder();
+      
+      sb.append('(');
+      sb.append(name);
+      
+      for (Event e = alt; e != null; e = e.alt){
+        sb.append( " || ");
+        sb.append(e.name);
+      }
+      
+      for (Event e = next; e instanceof CheckEvent; e = e.next){
+        sb.append( " && ");
+        sb.append(e.name);        
+      }
+      
+      sb.append(')');
+      
+      return sb.toString();
+    }
+  }
+}
